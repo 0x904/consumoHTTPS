e27f1bdac4c033903cbd68694dc3868cfc485e87
yo <causawey69420@gmail.com>
2026-03-16 21:51:55 +0100

    commit

--- src/Principal/ServicioApi.java
+++ src/Principal/ServicioApi.java
@@ -42,7 +42,7 @@ public class ServicioApi
 				in.close();
 				
 				JSONObject json = new JSONObject(sb.toString());
-				// ... pa seguir pushaasdadsas
+				// ... pa seguir puhhhhhh
 			}
 			
 			conexion.disconnect();
