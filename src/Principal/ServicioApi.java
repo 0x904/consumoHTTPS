218654fb1980aa91848c932700f06da5c0348a77
yo <causawey69420@gmail.com>
2026-03-16 19:31:00 +0100

    primer commit

---
+++ src/Principal/ServicioApi.java
@@ -0,0 +1,63 @@
+package Principal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.MalformedInputException;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.json.JSONObject;
+
+
+public class ServicioApi
+{
+	public static String obtenerDatos()
+	{
+		int codigo;
+		InputStreamReader flujoDeEntrada;
+		BufferedReader in;
+		StringBuilder sb = new StringBuilder();
+		String linea;
+
+		try
+		{
+			URL url = new URL("https://restcountries.com/v3.1/all?fields=name,capital,region,population");
+			HttpsURLConnection conexion = (HttpsURLConnection) url.openConnection();
+
+			conexion.setRequestMethod("GET");
+			conexion.setRequestProperty("User-Agent", "Java-HTTPS-Client");
+
+			codigo = conexion.getResponseCode();
+
+			System.out.println(codigo);
+
+			if (codigo==200)
+			{
+				in = new BufferedReader(
+						new InputStreamReader(conexion.getInputStream()));
+
+		        while ((linea = in.readLine()) != null) sb.append(linea);
+
+				in.close();
+				
+				JSONObject json = new JSONObject(sb.toString());
+				// ... pa seguir
+			}
+			
+			conexion.disconnect();
+
+		}
+		catch(MalformedInputException e)
+		{
+			System.out.println("malformao");
+		}
+		catch(IOException e)
+		{
+			System.out.println("io");
+		}
+
+		return "";
+	}
+}
